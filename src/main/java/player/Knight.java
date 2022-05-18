b2ddff84a40624751f1b360b7ff981d29cf328e6
Jack Dickson <jackowendickson@gmail.com>
2022-05-18 15:18:18 +0100

    Added knight

--- src/main/java/player/Knight.java
+++ src/main/java/player/Knight.java
@@ -1,4 +1,23 @@
 package player;
 
-public class Knight {
+import weapons.Weapon;
+
+public class Knight extends Fighter{
+
+    private int armour;
+
+    public Knight(String name, int healthPoints, Weapon weapon) {
+        super(name, healthPoints, weapon);
+        this.armour = 2;
+    }
+
+    public void takeDamage(int attack){
+        int modifiedAttack = attack - 2;
+        if (modifiedAttack <= 0){
+            //nothing happens, no damage
+        } else {
+            receiveDamage(modifiedAttack);
+        }
+
+    }
 }
--- src/test/java/KnightTest.java
+++ src/test/java/KnightTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+import player.Knight;
+import weapons.Weapon;
+
+import static org.junit.Assert.assertEquals;
+
 public class KnightTest {
+
+    Knight knight;
+
+    @Before
+    public void before(){
+        knight = new Knight("Larry", 20, Weapon.SWORD);
+    }
+
+    @Test
+    public void canReceiveReducedDamage(){
+        knight.takeDamage(5);
+        assertEquals(17, knight.getHealthPoints());
+    }
+
+    @Test
+    public void cannotGetHealedByAttacks(){
+        knight.takeDamage(1);
+        assertEquals(20, knight.getHealthPoints());
+    }
 }
